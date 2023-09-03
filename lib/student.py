3678de902d51661f5f6e775a963fc3951cb8ec3e
+ <+>
2023-09-03 23:53:00 +0300

    done

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,18 @@
 from user import User
 
 class Student(User):
+    def __init__(self):
+        self.knowledge=[]
     
-    def learn(self):
-        pass
+    def learn(self,mylist):
+        self.knowledge.append(mylist)
+        return self.knowledge
+    
+
+
+kimemia=Student()
+print(kimemia.learn("kimemia"))
+
+myname=[]
+myname.append(["me","again"])
+print(myname)    
